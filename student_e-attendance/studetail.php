3098e8c2f10aa40261e8fa7ed5cf916ffd84eee5
njwaliya <147322747+njwaliya@users.noreply.github.com>
2023-10-26 10:54:03 +0800

    Add files via upload

---
+++ student_e-attendance/studetail.php
@@ -0,0 +1,100 @@
+<html>
+    <head>
+        <title>STUDENT DETAILS</title>
+        <link rel="stylesheet" href="header.css">
+        <style>
+             body{
+            background-color: #FFFDE7;
+            background-repeat: no-repeat; 
+  	        background-size: cover;
+            font-family: "Montserrat", sans-serif;
+            font-size: 1rem;
+            font-weight: 500;
+            line-height: 1.5;
+            color: #212529;
+            text-align: center;
+             }
+        .mb-1, .my-1 {
+         margin-bottom: 0.25rem!important;}
+        </style>
+</head>
+<body> 
+    <div class="topnav">
+    <section>
+            <header>
+                <a href="#" class="logo"><img src="logokypj.png" width="200px" heigth="150px"></a>
+                <text = "Student E-Attendance">
+                    <div class="topnav" id="myTopnav">
+                <ul>
+                    <li><a href="logout.php">LOG OUT</a></li>
+                 <a href="javascript:void(0);" class="icon" onclick="myFunction()"></a>
+                </ul>
+            </header>
+        </section>
+    </div>
+<br>
+</body>
+
+<div class="container">
+<div class="row">
+  <div class="col-sm-2">
+  <div class="col-sm-8" style="border: 2px solid black;padding:5px; text-align: center;">
+   <h1>STUDENT DETAIL</h1>
+  <br><br><br>
+
+  <div class="form-group row d-flex align-items-center mb-1">
+
+  <label class="col-sm-2 form-control-label d-flex justify-content-lg-end">
+    <strong>FULL NAME :</strong>
+    </label>
+
+    <div class="col-lg-6">NUR NAJWA ALIEYA BINTI ROMZI</div>
+    </div>
+
+    <div class="form-group row d-flex align-items-center mb-1">
+  <label class="col-sm-2 form-control-label d-flex justify-content-lg-end">
+    <strong>NO MATRIC :</strong>
+    </label>
+    
+    <div class="col-lg-6">DDWD2021/060273</div>
+    </div>
+
+    <div class="form-group row d-flex align-items-center mb-1">
+  <label class="col-sm-2 form-control-label d-flex justify-content-lg-end">
+    <strong>NO IC :</strong>
+    </label>
+
+    <div class="col-lg-6">031204-14-0438</div>
+    </div>
+
+    <div class="form-group row d-flex align-items-center mb-1">
+  <label class="col-sm-2 form-control-label d-flex justify-content-lg-end">
+    <strong>NO TELEPHONE :</strong>
+    </label>
+
+    <div class="col-lg-6">013-8020153</div>
+    </div>
+
+    <div class="form-group row d-flex align-items-center mb-1">
+  <label class="col-sm-2 form-control-label d-flex justify-content-lg-end">
+    <strong>EMAIL :</strong>
+    </label>
+
+    <div class="col-lg-6">njwaliyaa@gmail.com</div>
+    </div>
+
+    <div class="form-group row d-flex align-items-center mb-1">
+  <label class="col-sm-2 form-control-label d-flex justify-content-lg-end">
+    <strong>QR CODE :</strong>
+    </label>
+
+    <div class="col-lg-6"></div>
+    </div>
+    <br><br><br><br><br>
+</div>
+</div>
+</div>
+</div>
+</html>
+<br><br>
+    <?php require('footer.php'); ?>
