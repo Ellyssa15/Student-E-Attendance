7d28f42345fc6818382c3908c3e9300bc9524a19
Ellyssa15 <147320813+Ellyssa15@users.noreply.github.com>
2023-11-05 02:54:17 +0800

    Update stuScan.php

--- student_e-attendance/stuScan.php
+++ student_e-attendance/stuScan.php
@@ -1,13 +1,14 @@
+<!DOCTYPE html>
 <html>
-    <head>
-        <title>STUDENT SCAN QR CODE</title>
-        <link rel="stylesheet" href="header.css">
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
-<script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> 
-        <style>
-             body{
+<head>
+    <title>STUDENT SCAN QR CODE</title>
+    <link rel="stylesheet" href="header.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/3.3.3/adapter.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/vue/2.1.10/vue.min.js"></script>
+    <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"> 
+    <style>
+        body{
             background-color: #FFFDE7;
             background-repeat: no-repeat; 
   	        background-size: cover;
@@ -27,59 +28,107 @@
         button:hover{
             background:#857F72
         }
-        </style>
-    </head>
-    <body>
-<div class="topnav">
-    <section>
+        .manual-input {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+    <div class="topnav">
+        <section>
             <header>
                 <a href="#" class="logo"><img src="logokypj.png" width="200px" heigth="150px"></a>
-                    <div class="topnav" id="myTopnav">
+                <div class="topnav" id="myTopnav">
             </header>
         </section>
     </div>
-    <br><br><br><br>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                <label>SCAN QR CODE</label>
-                    <video id="preview" width="100%"></video>
-                </div>
-                <div class="col-md-6">
-                    <label>QR CODE</label>
-                    <input type="text" name="text" id="text" readonyy="" placeholder="QR code" class="form-control">
-                    <br><br>
-                    <label>MANUALLY ENTER STUDENT QR CODE</label>
-                    <input type="number" name="qrtext" id="qrcode" placeholder="QR code" class="form-control">
-
-                </div>
+    <div class="container">
+        <h2>STUDENT SCAN QR CODE</h2>
+        <label>SUBJECT:</label>
+        <select name="subject" id="subject" class="form-control">
+            <option value="DDWD2653">DDWD2653</option>
+            <option value="DDWD3343">DDWD3343</option>
+            <option value="DDWD3723">DDWD3723</option>
+            <option value="DDWD3773">DDWD3773</option>
+            <option value="DDWD3783">DDWD3783</option>
+        </select>
+        <br>
+        <label>SCAN QR CODE:</label>
+        <input type="radio" name="scan" value="auto" checked> Auto
+        <input type="radio" name="scan" value="manual"> Manual
+        <br>
+        <div class="row">
+            <div class="col-md-6">
+                <video id="preview" width="100%"></video>
+            </div>
+            <div class="col-md-6">
+                <label>QR CODE</label>
+                <input type="text" name="text" id="text" readonly="" placeholder="QR code" class="form-control">
+                <br>
+                <input type="text" name="qrtext" id="qrcode" placeholder="QR code" class="form-control manual-input">
+                <button onclick="saveQRCode()">SAVE</button>
             </div>
         </div>
+    </div>
 
-        <script>
-           let scanner = new Instascan.Scanner({ video: document.getElementById('preview')});
-           Instascan.Camera.getCameras().then(function(cameras){
-               if(cameras.length > 0 ){
-                   scanner.start(cameras[0]);
-               } else{
-                   alert('No cameras found');
-               }
+    <script>
+        let scanner = null;
+        let scanMode = 'auto';
+        let preview = document.getElementById('preview');
+        let text = document.getElementById('text');
+        let qrcode = document.getElementById('qrcode');
+        let manualInput = document.querySelector('.manual-input');
 
-           }).catch(function(e) {
-               console.error(e);
-           });
+        function startScanner() {
+            scanner = new Instascan.Scanner({ video: preview });
+            Instascan.Camera.getCameras().then(function(cameras){
+                if(cameras.length > 0 ){
+                    scanner.start(cameras[0]);
+                } else{
+                    alert('No cameras found');
+                }
+            }).catch(function(e) {
+                console.error(e);
+            });
 
-           scanner.addListener('scan',function(c){
-               document.getElementById('text').value=c;
-           });
+            scanner.addListener('scan',function(c){
+                text.value = c;
+            });
+        }
 
-        </script>
-<br><br><br>
-<div class="button-container">
-    <a href="stuHome.php" class="centered-button">
-        <button>BACK</a></button> </a>
-</div>
-    </body>
-<br>
+        function stopScanner() {
+            scanner.stop();
+        }
+
+        function saveQRCode() {
+            let qrCode = qrcode.value;
+            // Send qrCode to server for saving
+            // ...
+            alert('QR code saved: ' + qrCode);
+        }
+
+        let scanRadio = document.getElementsByName('scan');
+        for (let i = 0; i < scanRadio.length; i++) {
+            scanRadio[i].addEventListener('change', function() {
+                scanMode = this.value;
+                if (scanMode === 'auto') {
+                    startScanner();
+                    manualInput.style.display = 'none';
+                } else {
+                    stopScanner();
+                    manualInput.style.display = 'block';
+                }
+            });
+        }
+
+        startScanner();
+    </script>
+
+    <div class="button-container">
+        <a href="lectHome.php" class="centered-button">
+            <button>BACK</button>
+        </a>
+    </div>
+</body>
 <?php require('footer.php'); ?>
 </html>
