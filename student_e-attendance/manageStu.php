f2484e080522fe9f3c92f23ffff3522bea904272
Ellyssa15 <147320813+Ellyssa15@users.noreply.github.com>
2023-11-27 02:02:16 +0800

    Update manageStu.php

--- student_e-attendance/manageStu.php
+++ student_e-attendance/manageStu.php
@@ -1,31 +1,32 @@
 <?php
-// Include database connection file
 include_once("connection.php");
 
 // Add student record
 if(isset($_POST['add'])) {
+    $studentId = $_POST['studentId'];
     $name = $_POST['name'];
+    $course = $_POST['course'];
     $noIC = $_POST['noIC'];
     $noTel = $_POST['noTel'];
-    $email = $_POST['email'];
-    $result = mysqli_query($connection, "INSERT INTO stu_detail(name,noIC,noTel,email) VALUES('$name','$noIC','$noTel','$email')");
+    $result = mysqli_query($connection, "INSERT INTO stu_detail(studentId,name,course,noIC,noTel) VALUES('$studentId','$name','$course','$noIC','$noTel')");
     header("Location: manageStu.php");
 }
 
 // Edit student record
 if(isset($_POST['update'])) {
+    $studentId = $_POST['studentId'];
     $name = $_POST['name'];
+    $course = $_POST['course'];
     $noIC = $_POST['noIC'];
     $noTel = $_POST['noTel'];
-    $email = $_POST['email'];
-    $result = mysqli_query($connection, "UPDATE stu_detail SET noIC='$noIC',noTel='$noTel',email='$email' WHERE name=$name");
+    $result = mysqli_query($connection, "UPDATE stu_detail SET name='$name', course='$course', noIC='$noIC',noTel='$noTel' WHERE studentId=$studentId");
     header("Location: manageStu.php");
 }
 
 // Delete student record
 if(isset($_GET['delete'])) {
     $name = $_GET['delete'];
-    $result = mysqli_query($connection, "DELETE FROM stu_detail WHERE name=$name");
+    $result = mysqli_query($connection, "DELETE FROM stu_detail WHERE studentId=$studentId");
     header("Location: manageStu.php");
 }
 ?>
@@ -98,29 +99,32 @@ if(isset($_GET['delete'])) {
         <h2>Student List</h2>
         <table>
             <tr>
+                <th>Student ID</th>
                 <th>Name</th>
+                <th>Course</th>
                 <th>IC Number</th>
                 <th>Telephone Number</th>
-                <th>Email</th>
                 <th>Actions</th>
             </tr>
             <?php
             require_once 'connection.php';
             $query = mysqli_query($connection,"select * from stu_detail");
             while($row = mysqli_fetch_array($query)){
+                $studentId = $row['studentId'];
                 $name = $row['name'];
+                $course = $row['course'];
                 $noIC = $row['noIC'];
                 $noTel = $row['noTel'];
-                $email = $row['email'];
             ?>
             <tr>
+                <td><?php echo $studentId; ?></td>
                 <td><?php echo $name; ?></td>
+                <td><?php echo $course; ?></td>
                 <td><?php echo $noIC; ?></td>
                 <td><?php echo $noTel; ?></td>
-                <td><?php echo $email; ?></td>
                 <td>
-                    <a href="stuedit.php?id=<?php echo $name; ?>"><button>EDIT</button></a>
-                    <a href="delete.php?id=<?php echo $name; ?>"><button>DELETE</button></a>
+                    <a href="stuEdit.php?id=<?php echo $studentId; ?>"><button>EDIT</button></a>
+                    <a href="deleteStu.php?id=<?php echo $studentId; ?>"><button>DELETE</button></a>
                 </td>
             </tr>
             <?php
@@ -128,7 +132,7 @@ if(isset($_GET['delete'])) {
             ?>
         </table>
         <br>
-        <td><a href="addstu.php?id=<?php echo $name; ?>"><button>ADD STUDENT</button></a></td>
+        <td><a href="addstu.php?id=<?php echo $studentId; ?>"><button>ADD STUDENT</button></a></td>
     </div>
 </body>
 <?php require('footer.php'); ?>
