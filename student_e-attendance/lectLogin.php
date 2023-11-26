5a68c4cc45c6aca5a03d9c027add0ef96ea1c154
Ellyssa15 <147320813+Ellyssa15@users.noreply.github.com>
2023-11-27 02:01:25 +0800

    Update lectLogin.php

--- student_e-attendance/lectLogin.php
+++ student_e-attendance/lectLogin.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>LOGIN</title>
+        <title>LECTURER LOGIN</title>
         <link rel="stylesheet" href="login.css">
         <style>
           h3 {
@@ -17,29 +17,34 @@
     </style>
     </head>    
     <?php
-    session_start();
-    include 'connection.php';
-    if (isset($_POST['submit'])) {
-        $username=$_POST['username'];
-        $password=$_POST['password'];
-        $query=mysql_query("select * from lecturerlogin where username='$username' and password='$password'");
-        $queryRow=mysql_num_rows($query);
-        if($queryRow==TRUE) {
-            $_SESSION['username']=$username;
-            header("location:lectHome.php");
-        }else {
-            echo "<script> alert ('Username atau Password Salah');
-            location='lectLogin.php'; </script>";
-        }
+session_start();
+include 'connection.php';
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM lecturerlogin WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($connection, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        $_SESSION['username'] = $username;
+        header("location: lectHome.php");
+    } else {
+        echo "<script> alert ('Username atau Password Salah');
+        location='lectLogin.php'; </script>";
     }
-    ?>
+}
+
+mysqli_close($connection);
+?>
    
    <br>
    <div class = "content">
    <br><h3>LECTURER LOG IN</h3>
         <form method="post">
-        <img src="login icon.png" class="pic"><input type="text" placeholder="username" name="username" required><br><br>
-        <img src="lock.png" class="pic"><input type="password" placeholder="password" name="password" required><br>
+        <img src="login icon.png" class="pic"><input type="text" name="username" placeholder="username" required><br><br>
+        <img src="lock.png" class="pic"><input type="password" name="password" placeholder="password" required><br>
                <br><input type="checkbox" name="remember"> Remember me <br>
                <br><input type="submit" name="submit" value="Log In"></form>
                 
