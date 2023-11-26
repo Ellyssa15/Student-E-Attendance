c1c6fe3cc593c70f9cd089e5f07aea765d22cc8b
Ellyssa15 <147320813+Ellyssa15@users.noreply.github.com>
2023-11-27 01:02:45 +0800

    Update adminLogin.php

--- student_e-attendance/adminLogin.php
+++ student_e-attendance/adminLogin.php
@@ -18,29 +18,35 @@
     </head> 
     <body>
 <div>
-    <?php
-    session_start();
-    include 'connection.php';
-    if (isset($_POST['submit'])) {
-        $username=$_POST['username'];
-        $password=$_POST['password'];
-        $query=mysql_query("select * from admin_login where username='$username' and password='$password'");
-        $queryRow=mysql_num_rows($query);
-        if($queryRow==TRUE) {
-            $_SESSION['username']=$username;
-            header("location:adminHome.php");
-        }else {
-            echo "<script> alert ('Username atau Password Salah');
-            location='adminLogin.php'; </script>";
-        }
+<?php
+session_start();
+include 'connection.php';
+
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $sql = "SELECT * FROM admin_login WHERE username = '$username' AND password = '$password'";
+    $result = mysqli_query($connection, $sql);
+
+    if (mysqli_num_rows($result) == 1) {
+        $_SESSION['username'] = $username;
+        header("location: adminHome.php");
+    } else {
+        echo "<script> alert ('Username atau Password Salah');
+        location='adminLogin.php'; </script>";
     }
-    ?>   
+}
+
+mysqli_close($connection);
+?>
+  
    <br>
    <div class = "content">
    <br><h3>ADMIN LOGIN</h3>
         <form method="post">
-        <img src="login icon.png" class="pic"><input type="text" placeholder="username" required><br><br>
-        <img src="lock.png" class="pic"><input type="password" placeholder="password" required><br>
+        <img src="login icon.png" class="pic"><input type="text" name="username" placeholder="username" required><br><br>
+        <img src="lock.png" class="pic"><input type="password" name="password" placeholder="password" required><br>
         <br><input type="checkbox" name="remember" require> Remember me <br>
         <br><br><input type="submit" name="submit" value="Log In"></form>
                 
